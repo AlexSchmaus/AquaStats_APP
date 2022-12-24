a5a8f819c0bc942d2261a8d45f428645ca2a603c
Alex Schmaus <94810049+AlexSchmaus@users.noreply.github.com>
2022-12-24 16:17:53 -0500

    testing API routes in svelte

--- src/routes/api/waterParameters/+server.js
+++ src/routes/api/waterParameters/+server.js
@@ -1,15 +1,34 @@
 import {DBUSER, DBPASS, DBADDR} from '$env/static/private'
-import {default as sql} from mssql
+import {default as sql} from "mssql"
+import { json } from "@sveltejs/kit"
 
-export function POST({request}) {
-
-    const config = {
-        user: DBUSER,
-        password: DBPASS,
-        server: DBADDR,
-        database: "AquaStats"
+const config = {
+    user: DBUSER,
+    password: DBPASS,
+    server: DBADDR,
+    database: "AquaStats",
+    options: {
+        trustServerCertificate: true
     }
+}
+
+
+export async function POST({request}) {
+
+    let db = await sql.connect(config)
+    let res = await db.request()
+        .input('json', sql.VarChar(4000), value)
+        .query('SELECT * FROM dbo.users')
 
     /* stuff */
-    return new Response()
+    return json(res)
+}
+
+export async function GET(){
+    
+    let db = await sql.connect(config)
+    let res = await db.request()
+        .query('SELECT * FROM dbo.users')
+    
+    return json(res)
 }
